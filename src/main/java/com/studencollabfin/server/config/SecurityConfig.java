905168bbb9c71b76c61cec7ac48d8b49c86f19c1
Taksh Bansod <takshinfiity@gmail.com>
2026-02-02 02:46:08 +0530

    Fix: Configure backend to use secure MONGO_URI

--- src/main/java/com/studencollabfin/server/config/SecurityConfig.java
+++ src/main/java/com/studencollabfin/server/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://tezzera.netlify.app"));
+                List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:3000",
+                        "https://tezzera.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
